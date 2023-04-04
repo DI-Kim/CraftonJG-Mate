5019103b9a862035418c7f697a93cbc0e9b4309f
박윤찬 <dbscks9793@gmail.com>
2023-04-04 23:20:08 +0900

    detail page

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def join():
 def main():
     return render_template('main.html')
 
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port = 5700, debug = True)
