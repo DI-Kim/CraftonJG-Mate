7b5128779d61091e2eebf6979c247507350ce2eb
bigperson <daein7076@gmail.com>
2023-04-04 21:53:24 +0900

    "login, join, main 프론트엔드 v1.00"

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port = 5700, debug = True)
