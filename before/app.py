6f40a26eb4d272178a89071ddfa3b5a52ae9ebbc
bigperson <daein7076@gmail.com>
2023-04-07 11:59:45 +0900

    final

--- before/app.py
+++ before/app.py
@@ -9,8 +9,8 @@ from time import time
 import config
 
 app = Flask(__name__)
-client = MongoClient(config.MONGO_DB, 27017)
-# client = MongoClient('localhost', 27017)
+# client = MongoClient(config.MONGO_DB, 27017)
+client = MongoClient('localhost', 27017)
 db = client.mate
 
 SECRET_KEY = config.SECRET_KEY
@@ -197,4 +197,4 @@ def joinGroup():
   
     
 if __name__ == '__main__':
-    app.run('0.0.0.0', port = 5000, debug = True)
+    app.run('0.0.0.0', port = 5600, debug = True)
